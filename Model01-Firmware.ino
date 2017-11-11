428c63b97cb258198161d4a73ac05062db95016d
Benjamin Gilbert <bgilbert@backtick.net>
2017-11-10 21:59:36 -0800

    Move NumLock to Any key; add Home/PgUp/PgDown/End on right column

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -121,10 +121,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   ___,           Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_KeypadNumLock, Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Home,
+   Key_Enter,         Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_PageUp,
+                      Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_PageDown,
+   Key_RightAlt,      Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_End,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
